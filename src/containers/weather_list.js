1abcbeda852191bf3955f1f593f1a109bab563a7
Thomas Neppelenbroek <thomas@newzoo.com>
2018-07-04 15:29:16 +0200

    complete

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines , SparlinesLine}
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lat, lon } = cityData.city.coord;
 
-        return(
+        console.log(temps);
+
+        return (
             <tr key={name}>
-                <td>{name}</td>
-                <td>
-                    <Sparklines height={120} width={180} data={temps}>
-                        <SparklinesLine color="red">
-
-                        </SparklinesLine>
-                    </Sparklines>
-                </td>
+                <td><GoogleMap lat={lat} lon={lon}/></td>
+                <td><Chart data={temps} color="orange" units="k" /></td>
+                <td><Chart data={pressures} color="green" units="hpa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
-        )
+        );
     }
 
     render() {
@@ -27,9 +29,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (k)</th>
+                        <th>Pressure (hpa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
